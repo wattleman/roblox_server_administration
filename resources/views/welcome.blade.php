021a9e1c1a75ed4f0871d4ee4579ce473fd8f30b
wattleman <wattleman101@gmail.com>
2019-04-19 16:05:14 -0500

    Added env.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>{{env('APP_ENV')}}</title>
+        <title>{{env('APP_NAME')}}</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    {{env('APP_ENV')}}
+                    {{env('APP_NAME')}}
                 </div>
 
                 <div class="links">
