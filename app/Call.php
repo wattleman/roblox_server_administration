ae1b93dc41172579426d4c7a9332fa07ec79b5d6
wattleman <wattleman101@gmail.com>
2019-04-20 22:27:19 -0500

    Fillable

--- app/Call.php
+++ app/Call.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Call extends Model
 {
-    //
+    protected $fillable = ["id", "place_id", "server_id", "caller", "reported_user", "call_details", "created_at", "updated_at"];
 }
