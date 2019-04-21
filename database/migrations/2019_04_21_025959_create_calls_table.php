c64e8fdc446c52db9666682a7ec649763cbda82d
wattleman <wattleman101@gmail.com>
2019-04-20 22:02:51 -0500

    Added calls

---
+++ database/migrations/2019_04_21_025959_create_calls_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCallsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('calls', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            $table->bigInteger('place_id')->unsigned()->unique();
+            $table->bigInteger('server_id')->unsigned()->unique();
+
+            $table->string('caller')->default('Unknown');
+
+            $table->string('reported_user')->default('N/A');
+            $table->longText('call_details')->nullable();
+
+            $table->timestamps();
+
+            $table->foreign('place_id')->references('place_id')->on('games')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('calls');
+    }
+}
