88f92910b9b133e2d149332b5b8e33424d0993bd
wattleman <wattleman101@gmail.com>
2019-04-20 22:34:03 -0500

    Fix

--- app/Game.php
+++ app/Game.php
@@ -9,9 +9,9 @@ class Game extends Model
 
     public function checkAuth($password){
         if($game->password == $password && ($game->active == 1)){
-            return response("Game authorized.")->header('Content-Type', 'text-plain');
+            return true;
         }else{
-            return response("Game-ID and/or Password is/are incorrect.")->header('Content-Type', 'text-plain');
+            return false;
         }
     }
 
