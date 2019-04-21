5c7d1b80816f9210f9638046faa79626f2fdb84e
wattleman <wattleman101@gmail.com>
2019-04-20 20:58:43 -0500

    Fix

--- app/Http/Controllers/ROBLOX_Requests/GameController.php
+++ app/Http/Controllers/ROBLOX_Requests/GameController.php
@@ -9,6 +9,12 @@ use App\Game;
 class GameController extends Controller
 {
     public function checkAuth(){
+
+        $_POST = json_decode(file_get_contents('php://input'), true);
+
+        $id = $_POST['gameID'];
+        $password = $_POST['password'];
+
         $game = Game::Where('place_id', $id)->where('password',$password)->first();
         if($game && ($game->active == 1)){
             return response("Game authorized.")->header('Content-Type', 'text-plain');
