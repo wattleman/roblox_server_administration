df5a1f2247432a7ba75356228a755356c76d66b3
wattleman <wattleman101@gmail.com>
2019-05-18 23:42:10 -0500

    Fix

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -5,6 +5,7 @@ namespace App\Console;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 use App\Server;
+use Carbon\Carbon;
 
 class Kernel extends ConsoleKernel
 {
